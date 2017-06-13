da5cf35673f06f833b4bb975442018d06f547aec
osbornq <qlosborn@gmail.com>
2017-06-13 02:06:01 -0700

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,152 @@
+/* Client-side JS for final assignment
+**
+** Recieve, store, and remove information
+**
+** Alter HTML Elements
+*/
+
+var newRatingButton = document.getElementById('new-rating-button');
+var cancelButton = document.getElementById('cancel-input');
+var acceptButton = document.getElementById('accept-input');
+
+/*Display modal to add new rating*/
+var showNewRatingModal = function()
+{
+	console.log("Show modal clicked");
+	var newRatingModalBackdrop = document.getElementById('new-rating-modal-backdrop');
+	var newRatingModal = document.getElementById('new-rating-modal');
+	
+	newRatingModalBackdrop.classList.remove('hidden');
+	newRatingModal.classList.remove('hidden');
+}
+
+/*Hide modal if cancelled or accepted, reset fields*/
+var hideNewRatingModal = function()
+{
+	console.log("Cancel modal called");
+	var newRatingModalBackdrop = document.getElementById('new-rating-modal-backdrop');
+	var newRatingModal = document.getElementById('new-rating-modal');
+	var nameInput = document.getElementById('name-input');
+	var titleInput = document.getElementById('title-input');
+	var scoreInput = document.getElementById('score-input');
+	
+	
+	newRatingModalBackdrop.classList.add('hidden');
+	newRatingModal.classList.add('hidden');
+	nameInput.value = '';
+	titleInput.value = '';
+	scoreInput.value = '--';
+}
+
+/*
+** Gets information from user to
+** add new rating. Calls store
+** function to add to database.
+*/
+var addNewRating = function()
+{
+	//If we're on the page that already has names (ratings page), give it the name from title
+	//This works because name-input is added dynamically.
+	if(document.getElementById('name-input')) 
+	{
+		var name = document.getElementById('name-input').value;
+	}
+	else
+	{
+		var name = document.getElementById('ratings-title').innerHTML;
+		name = name.substring(11,name.length);
+	}
+	
+	//put everything into cariables to pass to store function
+	var score = document.getElementById('score-input').value;
+	var title = document.getElementById('title-input').value;
+	var user = nameToUser(name);
+	
+	//if anything isn't filled in, make them fill it in
+	if(name === '' || score === '--' || title === '')
+	{
+		alert('Error. Some entry was left blank. Please make sure all fields are filled.');
+	}
+	else
+	{
+		storeData(user,name,title,score,function(err) {
+			if(err)
+			{
+				alert('Couldn\'t store data. Error in request: ' + error);
+			}
+			else
+			{}
+			
+		});
+	
+		hideNewRatingModal();
+	}
+}
+/*
+** Removes spaces from a name and
+** puts it all to lowercase to make url key
+**/
+var nameToUser = function(name)
+{
+	var user = name;
+	user = user.toString().split(' ').join('');
+	user = user.toString().toLowerCase();
+	return user;
+}
+
+/*
+** Store data in ratings.json
+** Return user to people page, effectively refreshing
+**
+*/
+var storeData = function(user,name,title,score,callback)
+{
+	var userURL = "/";
+	
+	var postReq = new XMLHttpRequest();
+	postReq.open('POST',userURL);
+	postReq.setRequestHeader('Content-Type','application/json');
+	
+	//after request loads, check for error and send back new error if true
+	postReq.addEventListener('load', function(event) {
+		var err;
+		if(event.target.status !== 200) {
+			error = event.target.response;
+		}
+		callback(err);
+	});
+	
+	
+	//create post body object to send to server.js post call
+	var postBody = {
+		user: user,
+		name: name,
+		title: title,
+		score: score
+	};
+	
+	postReq.send(JSON.stringify(postBody));
+	console.log("post request complete in index.js");
+	
+	//redirect to people page
+	window.location.href = '/people';
+}
+
+/*
+** This function alternates the colors of the ratings
+** in order to improve readability.
+**
+*/
+var alternateRatingsColors = function() {
+	var parentNode = document.getElementsByClassName('rating-list')[0];
+	var children = parentNode.children;
+	for(var i = 1; i < children.length; i+=2)
+	{
+		children[i].children[0].classList.add('rating-odd');
+	}
+}
+
+alternateRatingsColors();
+newRatingButton.addEventListener('click',showNewRatingModal);
+cancelButton.addEventListener('click',hideNewRatingModal);
+acceptButton.addEventListener('click',addNewRating);
--- server.js
+++ server.js
@@ -9,18 +9,20 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
-var ratings = require('./ratings.json');
+var ratingsData = require('./ratings.json');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
 
 //home page
 app.get('/', function(req, res, next) {
 	var pageArguments = {
-		
+		personPage: false
 	};
 	
 	res.render('home',pageArguments);
@@ -31,7 +33,10 @@ app.get('/', function(req, res, next) {
 app.get('/people', function(req, res, next) {
 	
 	var pageArguments = {
-		user: ratings
+		user: ratingsData,
+		
+		//checks if you're on a page with ratings already so modal does not request name
+		personPage: false
 	};
 
 	res.render('peoplelist',pageArguments);
@@ -43,8 +48,7 @@ app.get('/people', function(req, res, next) {
 app.get('/ratings/:person', function(req, res, next) {
 	console.log(req.params);
 	var person = req.params.person;
-	var rating = ratings[person];
-	
+	var rating = ratingsData[person];
 	if(rating)
 	{
 		var pageArguments = {
@@ -52,7 +56,8 @@ app.get('/ratings/:person', function(req, res, next) {
 			ratings: rating.ratings,
 			title: rating.ratings.title,
 			score: rating.ratings.score,
-			pageTitle: "Ratings of " + rating.name
+			pageTitle: "Ratings of " + rating.name,
+			personPage: true
 		};
 	
 		res.render('ratings',pageArguments);
@@ -63,8 +68,88 @@ app.get('/ratings/:person', function(req, res, next) {
 	}
 });
 
+app.post('/',function(req, res, next) {
+	console.log(req.body.user);
+	if(req.body && req.body.title && req.body.score)
+	{
+		
+		for(var i in ratingsData)
+		{
+			if(req.body.user === JSON.stringify(ratingsData[i]))
+			{
+				req.body.name = ratingsData[i].name;
+				req.body.user = JSON.stringify(ratingsData[i]);
+			}
+		}
+		console.log(req.body.user);
+		//create sub-object and push onto the ratings object
+		var userRating = {
+			title: req.body.title,
+			score: req.body.score
+		};
+		
+		if(ratingsData[req.body.user])
+		{
+			var ratingsObj = ratingsData[req.body.user].ratings;
+		}
+		else
+		{
+			var ratingsObj = [];
+		}
+		ratingsObj.push(userRating);
+		console.log(ratingsObj);
+		
+		var rating = {
+			user: obj = {
+				name: req.body.name,
+				ratings: ratingsObj
+			}
+		};
+		
+			//convoluted method of adding things to JSON
+			
+			//make sure the rating object is a string
+			var ratingString = JSON.stringify(rating);
+			
+			//change 'user' in json to actual username for url
+			ratingString = ratingString.replace(ratingString.substring(2,6),req.body.user.toString());
+			
+			//remove extra brackets from the string so it can be added to json
+			ratingString = ratingString.slice(1,-1);
+			
+			//extra cleaning up
+			ratingsData = JSON.stringify(ratingsData);
+			ratingsData = ratingsData.slice(0,-1);
+			ratingsData = ratingsData + "," + ratingString + "}";
+			ratingsData = JSON.parse(ratingsData);
+			
+			//writes to ratings.json. returns 500 error if there's a failure.
+			fs.writeFile('./ratings.json',JSON.stringify(ratingsData), function(err) {
+				if(err)
+				{
+					console.log('error after writefile');
+					res.status(500).send("Unable to save rating.");
+				}
+				else
+				{
+					//successfully written
+					console.log("Success");
+					res.status(200).send();
+				}
+			});
+				
+	}
+	else
+	{
+		console.log('error in last else');
+		res.status(400).send("Something missing from entry");
+	}
+});
+
+//serve public directories
 app.use(express.static(path.join(__dirname, 'public')));
 
+//404 if page DNE
 app.get('*', function (req, res) {
   res.status(404).render('404');
 });
